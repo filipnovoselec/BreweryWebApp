24e6a67e202bc8f81704a48f315f868de4371438
Filip Novoselec <filip.novoselec@gmail.com>
2016-09-17 21:13:38 +0200

    Database connection and views

--- BreweryApp/BreweryApp/App/app.js
+++ BreweryApp/BreweryApp/App/app.js
@@ -1,14 +1,32 @@
-﻿(function () {
-    'use strict';
+﻿var app = angular.module('app', ['ui.router', 'ngAnimate', 'ngRoute']);
 
-    angular.module('app', [
-        // Angular modules
-        'ngAnimate',
-        'ngRoute'
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
 
-        // Custom modules
+    $stateProvider
+        .state('home',
+        {
+            url: '/',
+            templateUrl: '/Views/Homepage.html',
+            controller: '/App/homepageController'
+        })
+        .state('login',
+        {
+            url: '/login',
+            templateUrl: '/Views/Login.html',
+            controller: '/App/loginController'
+        })
+        .state('recipe',
+        {
+            url: '/recipe/{id}',
+            templateUrl: '/Views/Recipe.html',
+            controller: '/App/recipeController'
+        })
+        .state('beer',
+        {
+            url: '/beer',
+            templateUrl: '/Views/Beer.html',
+            controller: '/App/beer'
+        });
+});
 
-        // 3rd Party Modules
-        
-    ]);
-})();
